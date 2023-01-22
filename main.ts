6162e5e2703c7bc52015533324e984622ce0e53f
RobQuincey <rpquincey@hotmail.com>
2023-01-22 15:39:02 +0000

    Simplify signal mapping

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
     signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    signal = Math.map(signal, -95, -42, 0, 9)
     led.plotBarGraph(
-    Math.map(signal, -95, -42, 0, 9),
+    signal,
     9
     )
-    if (signal < -80) {
+    if (signal < 4) {
         music.playTone(131, music.beat(BeatFraction.Sixteenth))
-    } else if (signal < -50) {
+    } else if (signal < 7) {
         music.playTone(349, music.beat(BeatFraction.Sixteenth))
     } else {
         music.playTone(988, music.beat(BeatFraction.Sixteenth))
